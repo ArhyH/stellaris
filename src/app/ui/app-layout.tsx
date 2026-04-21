2f632dc3dd718064e549f30a3e7407100da13454
Nikola Horbach <44953799+ArhyH@users.noreply.github.com>
2026-04-21 17:45:05 +0300

    [S-X] Add colors (#1)

--- src/app/ui/app-layout.tsx
+++ src/app/ui/app-layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 import { Header } from '@/widgets/header/ui/Header';
 import { Sidebar } from '@/widgets/sidebar/ui/Sidebar';
-import styles from '../ui/style.module.scss';
+import styles from './style.module.scss';
 
 const AppLayout = () => {
   return (
