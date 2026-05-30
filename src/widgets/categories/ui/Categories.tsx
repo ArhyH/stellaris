3d366c8f57bc97a364a297c96d0a19e3bc920ac8
Nick Horbach <creat.photo.art@gmail.com>
2026-05-30 17:41:32 +0300

    [Categories] Add filter component

--- src/widgets/categories/ui/Categories.tsx
+++ src/widgets/categories/ui/Categories.tsx
@@ -1,20 +1,31 @@
 import styles from './style.module.scss';
 import { CategoryItem as CategoryItemType } from '../model/types';
 import { CategoryItem } from './CategoryItem';
+import { SegmentedControl } from '@/shared/ui/SegmentedControl';
 
 type CategoriesProps = {
   categories: CategoryItemType[];
 };
 
+const FILTERS = [
+  { label: 'All', value: 'all' },
+  { label: 'Income', value: 'income' },
+  { label: 'Expense', value: 'expense' },
+];
+
 const Categories = (props: CategoriesProps) => {
   const { categories } = props;
 
   return (
-    <ul className={styles.categories}>
-      {categories.map((category) => {
-        return <CategoryItem data={category} key={category.categoryId} />;
-      })}
-    </ul>
+    <div className={styles.categories__wrapper}>
+      <SegmentedControl options={FILTERS} defaultValue={FILTERS[0].value} />
+
+      <ul className={styles.categories}>
+        {categories.map((category) => {
+          return <CategoryItem data={category} key={category.categoryId} />;
+        })}
+      </ul>
+    </div>
   );
 };
 
